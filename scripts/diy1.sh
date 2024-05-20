c37a929bb16b8533f5fa95fb043ed0f81ba64c43
iamsxm <45477640+iamsxm@users.noreply.github.com>
2024-05-20 13:31:31 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -1,7 +1,7 @@
 #添加helloworld
 echo 'src-git luci-app-ssr-plus https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #添加easymesh
-echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh.git' >> feeds.conf.default
+echo 'src-git luci-app-easymesh https://github.com/ntlf9t/luci-app-easymesh.git' >> feeds.conf.default
 #添加luci-app-adguardhome
 echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
 # istore
